98895c6909890f619abf8d403e13cc855df0cab4
stevenpfiggins <sfiggins43@gmail.com>
2018-11-19 13:41:39 -0500

    Add project files.

---
+++ RestaurantRater_MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RestaurantRater_MVC.Startup))]
+namespace RestaurantRater_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
