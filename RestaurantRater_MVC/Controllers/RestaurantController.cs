676eb3706db9cb2fcdae8e8839ee2e94b8368668
stevenpfiggins <sfiggins43@gmail.com>
2018-11-20 10:11:25 -0500

    Initial Commit

---
+++ RestaurantRater_MVC/Controllers/RestaurantController.cs
@@ -0,0 +1,38 @@
+﻿using RestaurantRater_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RestaurantRater_MVC.Controllers
+{
+    public class RestaurantController : Controller
+    {
+        private RestaurantDBContext db = new RestaurantDBContext();
+        // GET: Restaurant
+        public ActionResult Index()
+        {
+            return View(db.Restaurants.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        //POST: Restaurant/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "RestaurantID,Name")] Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Restaurants.Add(restaurant);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(restaurant);
+        }
+    }
+}
---
+++ RestaurantRater_MVC/Models/Restaurant.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantRater_MVC.Models
+{
+    public class Restaurant
+    {
+        [Key]
+        public int RestaurantID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Address { get; set; }
+        public float Rating { get; set; }
+    }
+
+    public class RestaurantDBContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
